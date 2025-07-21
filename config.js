cacdbe96b0d64b774166e3dbe035f441b1eed78f
sbuck4 <spencer.buck4@gmail.com>
2025-07-21 17:38:22 -0600

    added config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const emailConfig = {
+    publicKey: 'Ma0bE4oRLIsJ8PR5t',
+    serviceId: 'service_wlhpax2',
+    templateId: 'template_98mlqam'
+};
+
+const analyticsConfig = {
+    measurementId: 'G-8PQR6C81YQ'  // Replace with your actual GA4 Measurement ID
+};
